b2e4160672ee9177848f8fd0ab6c699910c21a24
Neko-Sato <hisatosi35350@gmail.com>
2023-07-28 06:58:45 +0900

    u

--- srcs/sort_utils.c
+++ srcs/sort_utils.c
@@ -6,11 +6,12 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 18:15:46 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/07/03 23:25:27 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/07/12 16:09:50 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
+#include "utils.h"
 
 int	is_sorted(t_stack *stack, int target, int descending)
 {
@@ -30,24 +31,40 @@ int	is_sorted(t_stack *stack, int target, int descending)
 	return (ret);
 }
 
-int	get_pivot(t_stack *stack, int target)
+int	get_pivot(t_stack *stack, int target, int reverse)
 {
-	if (!target)
-		return ((int)(unsigned long)(void *)&stack);
-	return (*stack->tail->value);
+	int		i;
+	double	n;
+
+	i = target;
+	n = 0;
+	while (i--)
+	{
+		if (reverse)
+			stack = stack->head;
+		else
+			stack = stack->tail;
+		n += *(int *)stack->value / (double)target;
+	}
+	return ((int)n);
 }
 
-t_stack	*stack_find(t_stack *stack, int target, int (*fun)(int, void *),
-		void *args)
+t_stack	*stack_find(t_stack *stack, int target, t_callback callback,
+		int reverse)
 {
-	t_stack *temp;
-	
-	temp = stack->tail;
-	while (target-- && temp != stack)
+	t_stack	*temp;
+
+	temp = stack;
+	while (1)
 	{
-		if (fun(*temp->value, args))
+		if (reverse)
+			temp = temp->head;
+		else
+			temp = temp->tail;
+		if (!(target-- && temp != stack))
+			break ;
+		if (callback.fun(temp->value, callback.args))
 			return (temp);
-		temp = temp->tail;
 	}
 	return (NULL);
 }
