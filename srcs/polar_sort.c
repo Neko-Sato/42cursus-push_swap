74ea49d5e45922cd61493d31505ea12d852c2cc0
Neko-Sato <hisatosi35350@gmail.com>
2023-07-28 05:41:07 +0900

    p

--- srcs/polar_sort.c
+++ srcs/polar_sort.c
@@ -6,38 +6,43 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 18:31:26 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/07/04 08:20:41 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/07/11 20:15:33 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "stack.h"
 
-static void	need_sort(t_stackset *stackset, int target[2], int *flag);
+static int	check_do_s(t_stackset *stackset, int flag);
+static void	update_flag(t_stackset *stackset, int target[2], int *flag);
 
-//	Sort when the sort target is 3 or less.
-int	polar_sort(t_stackset *stackset, int target[2])
+void	polar_sort(t_stackset *stackset, int target[2])
 {
 	int	flag;
 
-	flag = (1 < target[0] && target[0] <= 3) << 1 | (1 < target[1]
-			&& target[1] <= 3);
-	if (!flag)
-		return (0);
-	need_sort(stackset, target, &flag);
-	do_s_(stackset, flag);
-	need_sort(stackset, target, &flag);
-	do_rr_(stackset, flag);
-	do_s_(stackset, flag);
+	flag = (target[0] <= 3) << 1 | (target[1] <= 3);
+	update_flag(stackset, target, &flag);
+	do_s_(stackset, check_do_s(stackset, flag));
+	update_flag(stackset, target, &flag);
 	do_r_(stackset, flag);
-	need_sort(stackset, target, &flag);
 	do_s_(stackset, flag);
-	if ((1 < target[0] && target[0] <= 3) ^ (1 < target[1] && target[1] <= 3))
-		bisection_sort(stackset, , );
-	return (1);
+	do_rr_(stackset, flag);
+	update_flag(stackset, target, &flag);
+	do_s_(stackset, check_do_s(stackset, flag));
 }
 
-static void	need_sort(t_stackset *stackset, int target[2], int *flag)
+static int	check_do_s(t_stackset *stackset, int flag)
+{
+	if (flag & 0b10)
+		flag &= (*(int *)stackset->a.tail->value
+				> *(int *)stackset->a.tail->tail->value) << 1;
+	if (flag & 0b01)
+		flag &= (*(int *)stackset->b.tail->value
+				< *(int *)stackset->b.tail->tail->value);
+	return (flag);
+}
+
+static void	update_flag(t_stackset *stackset, int target[2], int *flag)
 {
 	int	temp;
 
